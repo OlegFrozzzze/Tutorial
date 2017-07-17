13088d051e43b57a24d641d982dc95f10a6fabb2
Frozzzze <ji900@yandex.ru>
2017-07-17 19:40:36 +0300

    Выполнил ещё 1 задание

--- src/Tutorial.java
+++ src/Tutorial.java
@@ -1,8 +1,18 @@
+import java.security.MessageDigest;
 /**
  * Created by ОЛЕГ on 17.07.2017.
  */
 public class Tutorial {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("It's alive! It's alive!");
+        Tutorial();
+    }
+    public static void Tutorial() throws Exception {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] digest = md.digest("abracadabra".getBytes("UTF-8"));
+        for (byte b : digest) {
+            System.out.printf("%02x", b);
+        }
+
     }
 }
