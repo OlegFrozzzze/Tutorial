4a0bd5b892b270deb245a9c612040f58769a0e66
Frozzzze <ji900@yandex.ru>
2017-07-19 02:17:14 +0300

    Поправка

--- src/Tom1/TaskFour.java
+++ src/Tom1/TaskFour.java
@@ -1,12 +1,10 @@
 package Tom1;
-
 import java.security.MessageDigest;
 
 /**
  * Created by ОЛЕГ on 18.07.2017.
  */
 public class TaskFour {
-
     public static void Tutorial() throws Exception {
         MessageDigest md = MessageDigest.getInstance("MD5");
         byte[] digest = md.digest("abracadabra".getBytes("UTF-8"));
--- src/Tutorial.java
+++ src/Tutorial.java
@@ -3,20 +3,16 @@ import Tom1.TaskTwo;
 import Tom1.TaskThee;
 import Tom1.TaskFour;
 import Tom1.TaskFive;
-/**
- * Created by ОЛЕГ on 17.07.2017.
- */
+
 public class Tutorial {
     public static void main(String[] args) throws Exception {
-        System.out.println("It's alive! It's alive!");
         TaskFive.Simple();
         TaskFour.Tutorial();
         TaskOne.charExpression();
         TaskTwo.isPowerOfTwo();
         TaskThee.flipBit();
+        System.out.println("It's alive! It's alive!");
     }
-
-
 }
 
 
