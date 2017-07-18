4f79ce5e59fb0f0b8fe71948702528e75224b966
Frozzzze <ji900@yandex.ru>
2017-07-18 20:29:52 +0300

    Выполнил задание на 3 уровне первая

--- src/Tutorial.java
+++ src/Tutorial.java
@@ -5,17 +5,23 @@ import java.security.MessageDigest;
 public class Tutorial {
     public static void main(String[] args){
         System.out.println("It's alive! It's alive!");
+        charExpression();
+        }public static char charExpression() {
+        int a = 1;
+        System.out.println((char)('\\'+a));
+        return (char)('\\'+a);
+
        // Simple();
        // Tutorial();
-        flipBit();
-        
+        //flipBit();
+
     }
-    public static int flipBit() {
-        int value = 2;
-        int bitIndex = 6;
-        System.out.println(value^(1 << bitIndex-1)); // put your implementation here
-        return value^(1 << bitIndex-1);
-    }
+//    public static int flipBit() {
+//        int value = 2;
+//        int bitIndex = 6;
+//        System.out.println(value^(1 << bitIndex-1)); // put your implementation here
+//        return value^(1 << bitIndex-1);
+//    }
 //    public static void Tutorial() throws Exception {
 //        MessageDigest md = MessageDigest.getInstance("MD5");
 //        byte[] digest = md.digest("abracadabra".getBytes("UTF-8"));
