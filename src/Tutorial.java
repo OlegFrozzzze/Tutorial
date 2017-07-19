51ea0814134f459c48203d40bd4718e8e0f9117e
Frozzzze <ji900@yandex.ru>
2017-07-19 15:23:47 +0300

    Поправка

--- src/Tutorial.java
+++ src/Tutorial.java
@@ -1,16 +1,8 @@
-import Tom1.TaskOne;
-import Tom1.TaskTwo;
-import Tom1.TaskThee;
-import Tom1.TaskFour;
-import Tom1.TaskFive;
+import Tom1.*;
 
 public class Tutorial {
     public static void main(String[] args) throws Exception {
-        TaskFive.Simple();
-        TaskFour.Tutorial();
-        TaskOne.charExpression();
-        TaskTwo.isPowerOfTwo();
-        TaskThee.flipBit();
+        TaskAll.Hello();
         System.out.println("It's alive! It's alive!");
     }
 }
