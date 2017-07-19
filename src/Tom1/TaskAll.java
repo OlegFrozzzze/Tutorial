3c8ed6f18815c4db35179bd6a1522dbb7d99efde
Frozzzze <ji900@yandex.ru>
2017-07-19 16:55:26 +0300

    Поправка

--- src/Tom1/TaskAll.java
+++ src/Tom1/TaskAll.java
@@ -1,11 +1,12 @@
 package Tom1;
 
 public class TaskAll {
-    public static void Hello()throws Exception{
+    public static void Hello() throws Exception {
         TaskOne.charExpression();
         TaskTwo.isPowerOfTwo();
         TaskThee.flipBit();
         TaskFive.Simple();
         TaskFour.Tutorial();
+        TaskSix.Def();
     }
 }
---
+++ src/Tom1/TaskSix.java
@@ -0,0 +1,9 @@
+package Tom1;
+
+public class TaskSix {
+    public static void Def() {
+        System.out.println("A" + 12);
+        System.out.println('A' + "12");
+        System.out.println("A" + ('\t' + '\u0003'));
+    }
+}
