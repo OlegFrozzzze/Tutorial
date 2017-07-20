160bf9a146e2a87d11fbac8a714cfdc5e1f4705c
Frozzzze <ji900@yandex.ru>
2017-07-20 22:33:23 +0300

    Поправка

--- src/HomeWork/AllTask.java
+++ src/HomeWork/AllTask.java
@@ -3,5 +3,6 @@ package HomeWork;
 public class AllTask {
     public static void allTask() {
       RegexTest.Regex();
+      TaskJavaRush.Task();
     }
 }
---
+++ src/HomeWork/TaskJavaRush.java
@@ -0,0 +1,10 @@
+package HomeWork;
+
+public class TaskJavaRush {
+    public static void Task() {
+        int i;
+        for (i = 0; i < 100; i++) {
+            System.out.println(i + "wfjwe");
+        }
+    }
+}
