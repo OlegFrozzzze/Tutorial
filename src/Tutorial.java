b7d33b1a8bebd69457730c5b33e44ea71fd1c542
Frozzzze <ji900@yandex.ru>
2017-07-20 23:59:55 +0300

    Поправка

--- src/Tutorial.java
+++ src/Tutorial.java
@@ -1,12 +1,14 @@
 import Tom1.*;
 import HomeWork.*;
+import NewTask.*;
 
 public class Tutorial {
     public static void main(String[] args) throws Exception {
-      //  TaskAll.Hello();
-//        AllTask.allTask();
-        }
+        //  TaskAll.Hello();
+//      AllTask.allTask();
+        // Game.tetris();
     }
+}
 
 
 
