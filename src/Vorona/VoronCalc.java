9a124026cb9d50a41018d7b902774391d74a5797
Frozzzze <ji900@yandex.ru>
2017-07-21 23:45:52 +0300

    Подготавил комоненты объекта

--- src/Vorona/VoronCalc.java
+++ src/Vorona/VoronCalc.java
@@ -11,5 +11,12 @@ public class VoronCalc extends JFrame{
     private JButton addCrow; // кнопка для добавления вароны
     private JButton removeCrow; // Кнопка для удоления вороны
 
+    public VoronCalc(){
+        super("Crow colculator");
 
+        //Подготавливаю комоненты объекта
+        countLabel =  new JLabel("Crow:" + voron); // Создаём объект с текстом + колличество ворон
+        addCrow = new JButton("Add Crow"); // Добовляем кнопку и текстом
+        removeCrow = new JButton("Remove Crow"); // Добовляем кнопку и текстом
+    }
 }
