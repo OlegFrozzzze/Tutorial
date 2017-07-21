5eeb253060ac3a8f9507bc8b0eb51f322e485e43
Frozzzze <ji900@yandex.ru>
2017-07-21 23:39:26 +0300

    ПРограмма для посчёта ворон =)

---
+++ src/Vorona/VoronCalc.java
@@ -0,0 +1,15 @@
+package Vorona;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+//Создаю программу для посчёта ворон
+
+public class VoronCalc extends JFrame{
+    private int voron = 0; // Колличество ворон
+    private JLabel countLabel; //Для текста
+    private JButton addCrow; // кнопка для добавления вароны
+    private JButton removeCrow; // Кнопка для удоления вороны
+
+
+}
