d250ad125744034b0efbb124da169d5737299a66
Frozzzze <ji900@yandex.ru>
2017-07-21 22:35:15 +0300

    Создал окно

--- src/NewTask/Game.java
+++ src/NewTask/Game.java
@@ -1,7 +1,11 @@
 package NewTask;
+import javax.swing.*;
 
-public class Game {
-    public static void tetris(){
-        //сдесь будет игра тетрис
+//сдесь будет игра тетрис
+public class Game extends JFrame {
+    public Game(){
+        super("Me First Window"); // Заголовок Окна
+        setBounds(100, 100, 200, 200); //выстовляем размер окна
+
     }
 }
