56d6c10bade25970c2d59402e02a3d38a3a933ac
Frozzzze <ji900@yandex.ru>
2017-07-22 23:25:48 +0300

    Поправка в pakage Vorona Слздаю блокнот Note

---
+++ src/Note/Note.java
@@ -0,0 +1,13 @@
+package Note;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Note extends JFrame{
+    MenuBar menuBar = new MenuBar();
+    private MenuItem menuOpen; //Элемент меню
+    private MenuItem menuSave;//Элемент меню
+    private MenuItem menuExit;//Элемент меню
+    private MenuItem menuAbout;//Элемент меню
+    private JTextArea theText; //Поле ввода текста
+}
