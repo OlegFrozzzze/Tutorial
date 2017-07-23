a956611c348632c9659666bb4d7d1803178216e5
Frozzzze <ji900@yandex.ru>
2017-07-23 22:11:06 +0300

    Запустил Calculator

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -3,8 +3,12 @@ package Calculator;
 import javax.swing.*;
 
 public class Calculator extends JFrame{
-    public Calculator(){
+    public  Calculator(){
         super ("Calculator");
-        
+        setBounds(700, 300, 500, 500);
+        setVisible(true);
+    }
+    public static void CalculatorMain(){
+        Calculator start =  new Calculator();
     }
 }
