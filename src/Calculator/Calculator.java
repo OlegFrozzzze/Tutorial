6aa44d487a7751d07c374c6cd74eff6d6065fa40
Frozzzze <ji900@yandex.ru>
2017-07-24 20:02:06 +0300

    Поправка

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -13,53 +13,31 @@ import javax.swing.*;
 // TODO 1.2.1) Кнопки должны быть от 1 до 0, + - / *, =, C
 
 public class Calculator extends JFrame {
-        JTextArea display = new JTextArea();//дисплей калькулятора
-        JPanel buttonPanel = new JPanel(new GridLayout(3, 5));//панель с кнопками
-        JButton button0 = new JButton("0");//кнопка 0
-        JButton button1 = new JButton("1");//кнопка 1
-        JButton button2 = new JButton("2");//кнопка 2
-        JButton button3 = new JButton("3");//кнопка 3
-        JButton button4 = new JButton("4");//кнопка 4
-        JButton button5 = new JButton("5");//кнопка 5
-        JButton button6 = new JButton("6");//кнопка 6
-        JButton button7 = new JButton("7");//кнопка 7
-        JButton button8 = new JButton("8");//кнопка 8
-        JButton button9 = new JButton("9");//кнопка 9
-        JButton buttonSum = new JButton("+");//кнопка +
-        JButton buttonBack = new JButton("C");//кнопка С
-        JButton buttonDivide = new JButton("/");//кнопка /
-        JButton buttonSub = new JButton("-");//кнопка -
-        JButton buttonMul = new JButton("*");//кнопка *
-        JButton buttonStart = new JButton("=");//кнопка =
+    JTextArea display = new JTextArea();//дисплей калькулятора
+    JPanel buttonPanel = new JPanel(new GridLayout(5, 3));//панель с кнопками
+    JButton button0 = new JButton("0");//кнопка 0
+    JButton button1 = new JButton("1");//кнопка 1
+    JButton button2 = new JButton("2");//кнопка 2
+    JButton button3 = new JButton("3");//кнопка 3
+    JButton button4 = new JButton("4");//кнопка 4
+    JButton button5 = new JButton("5");//кнопка 5
+    JButton button6 = new JButton("6");//кнопка 6
+    JButton button7 = new JButton("7");//кнопка 7
+    JButton button8 = new JButton("8");//кнопка 8
+    JButton button9 = new JButton("9");//кнопка 9
+    JButton buttonSum = new JButton("+");//кнопка +
+    JButton buttonBack = new JButton("C");//кнопка С
+    JButton buttonDivide = new JButton("/");//кнопка /
+    JButton buttonSub = new JButton("-");//кнопка -
+    JButton buttonMul = new JButton("*");//кнопка *
+    JButton buttonStart = new JButton("=");//кнопка =
 
-        int firstValue = 0;
-        String operation = "+";
+    int firstValue = 0;
+    String operation = "+";
+
     public Calculator() {
         super("Calculator");
-        setBounds(700, 300, 500, 500);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
-
-        add(display, BorderLayout.NORTH);
-        add(buttonPanel, BorderLayout.CENTER);
-        add(buttonStart, BorderLayout.SOUTH);
-        buttonPanel.add(button0);
-        buttonPanel.add(button1);
-        buttonPanel.add(button2);
-        buttonPanel.add(button3);
-        buttonPanel.add(button4);
-        buttonPanel.add(button5);
-        buttonPanel.add(button6);
-        buttonPanel.add(button7);
-        buttonPanel.add(button8);
-        buttonPanel.add(button9);
-        buttonPanel.add(buttonSum);
-        buttonPanel.add(buttonSub);
-        buttonPanel.add(buttonMul);
-        buttonPanel.add(buttonBack);
-        buttonPanel.add(buttonDivide);
-        setVisible(true);
-
+        setBounds(700, 300, 400, 500);
         button0.addActionListener(e -> display.setText(display.getText() + "0"));
         button1.addActionListener(e -> display.setText(display.getText() + "1"));
         button2.addActionListener(e -> display.setText(display.getText() + "2"));
@@ -71,12 +49,11 @@ public class Calculator extends JFrame {
         button8.addActionListener(e -> display.setText(display.getText() + "8"));
         button9.addActionListener(e -> display.setText(display.getText() + "9"));
 
-            //Удаление последнего действия
+        //Удаление последнего действия
         buttonBack.addActionListener(e -> {
             String temp = display.getText();
-            display.setText(temp.substring(0,temp.length()-1));
+            display.setText(temp.substring(0, temp.length() - 1));
         });
-
         //Добавил равно
         buttonSum.addActionListener(e -> {
             firstValue = Integer.valueOf(display.getText());
@@ -103,23 +80,45 @@ public class Calculator extends JFrame {
         });
         buttonStart.addActionListener(e -> {
             int secondValue = Integer.valueOf(display.getText());
-            if("+".equals(operation)){
-                display.setText((firstValue+secondValue)+"");
+            if ("+".equals(operation)) {
+                display.setText((firstValue + secondValue) + "");
             }
-            if("-".equals(operation)){
-                display.setText((firstValue-secondValue)+"");
+            if ("-".equals(operation)) {
+                display.setText((firstValue - secondValue) + "");
             }
-            if("*".equals(operation)){
-                display.setText((firstValue*secondValue)+"");
+            if ("*".equals(operation)) {
+                display.setText((firstValue * secondValue) + "");
             }
-            if("/".equals(operation)){
-                display.setText((firstValue/secondValue)+"");
+            if ("/".equals(operation)) {
+                display.setText((firstValue / secondValue) + "");
             }
             firstValue = 0;
             operation = "+";
         });
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        add(display, BorderLayout.NORTH);
+        add(buttonPanel, BorderLayout.CENTER);
+        add(buttonStart, BorderLayout.SOUTH);
+        buttonPanel.add(button1);
+        buttonPanel.add(button2);
+        buttonPanel.add(button3);
+        buttonPanel.add(button4);
+        buttonPanel.add(button5);
+        buttonPanel.add(button6);
+        buttonPanel.add(button7);
+        buttonPanel.add(button8);
+        buttonPanel.add(button9);
+        buttonPanel.add(buttonSum);
+        buttonPanel.add(button0);
+        buttonPanel.add(buttonSub);
+        buttonPanel.add(buttonMul);
+        buttonPanel.add(buttonBack);
+        buttonPanel.add(buttonDivide);
 
+        setVisible(true);
     }
+
     public static void CalculatorMain() {
         Calculator calculator = new Calculator();
     }
