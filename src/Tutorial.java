f48946ab28b2463a82a1ebc6cb660f9d57061533
Frozzzze <ji900@yandex.ru>
2017-07-25 23:07:30 +0300

    Поправил

--- src/Tutorial.java
+++ src/Tutorial.java
@@ -1,17 +1,13 @@
 import Calculator.Calculator;
+import NewTask.Game;
 import Note.*;
 import Vorona.crowsCalculation;
 
 public class Tutorial {
     public static void main(String[] args) throws Exception {
-       //  TaskAll.Hello();
-       // AllTask.allTask();
-       // Game.myWindowApp();
-         crowsCalculation.Vorona();
-//        Note.NoteBook();
-//        Calculator.CalculatorMain();
-//        System.out.println("It's Windows path: \"C:\\Program Files\\Java\\jdk1.7.0\\bin\"");
-//        System.out.println("It's Java string: \\\"C:\\\\Program Files\\\\Java\\\\jdk1.7.0\\\\bin\\\"");
+        //  TaskAll.Hello();
+        // AllTask.allTask();
+         Game.myWindowApp();
     }
 }
 
