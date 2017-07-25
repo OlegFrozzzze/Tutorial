59c99cc8d649bfd52659a610dcb1298bb796abe1
Frozzzze <ji900@yandex.ru>
2017-07-25 22:59:34 +0300

    Поправил

--- src/NewTask/Game.java
+++ src/NewTask/Game.java
@@ -1,5 +1,4 @@
 package NewTask;
-import Calculator.*;
 import javax.swing.*;
 import java.awt.*;
 
@@ -9,18 +8,14 @@ import static Vorona.crowsCalculation.Vorona;
 
 //сдесь будет игра тетрис
 public class Game extends JFrame {
-    private JButton calculator ;
-    private JButton crowsCalculation;
-    private JButton Note;
-
     public Game(){
         super("Me First Window"); // Заголовок Окна
         setBounds(100, 100, 200, 200); //выстовляем размер окна
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//закрываем окно и завершаем программу
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);//закрываем окно и завершаем программу
 
-        calculator = new JButton("Калькулятор");
-        crowsCalculation = new JButton("Счётчик ворон");
-        Note = new JButton("Блокнот");
+        JButton calculator = new JButton("Калькулятор");
+        JButton crowsCalculation = new JButton("Счётчик ворон");
+        JButton Note = new JButton("Блокнот");
 
         JPanel buttonsPanel = new JPanel(new GridLayout(3, 1));
         buttonsPanel.add(calculator);
