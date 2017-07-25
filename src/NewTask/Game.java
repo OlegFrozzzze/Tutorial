a7069734566ae2af4aec74a676270f6f94733240
Frozzzze <ji900@yandex.ru>
2017-07-25 21:46:56 +0300

    Добвил в программу по подсчёту ворон кнопку с калькулятором ))

--- src/NewTask/Game.java
+++ src/NewTask/Game.java
@@ -3,10 +3,21 @@ import javax.swing.*;
 
 //сдесь будет игра тетрис
 public class Game extends JFrame {
+    private JButton calculator ;
+    private JButton crowsCalculation;
+    private JButton Note;
+
     public Game(){
         super("Me First Window"); // Заголовок Окна
         setBounds(100, 100, 200, 200); //выстовляем размер окна
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//закрываем окно и завершаем программу
+
+        calculator = new JButton("Калькулятор");
+        crowsCalculation = new JButton("Счётчик ворон");
+        Note = new JButton("Блокнот");
+
+        
+
 
     }
     public static void myWindowApp(){
