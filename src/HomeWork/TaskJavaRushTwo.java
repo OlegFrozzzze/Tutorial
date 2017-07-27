3e573db451a2466351b119c14bd33edcd2748081
Frozzzze <ji900@yandex.ru>
2017-07-27 22:49:35 +0300

    Задача в JavaRush

---
+++ src/HomeWork/TaskJavaRushTwo.java
@@ -0,0 +1,10 @@
+package HomeWork;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class TaskJavaRushTwo {
+        public static void Rush() throws Exception {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            }
+}
