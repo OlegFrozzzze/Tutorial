603912336624d522986dba217d5ca512b3be12fc
Frozzzze <ji900@yandex.ru>
2017-07-28 22:47:53 +0300

    Создал новый класс

---
+++ src/Rush/RushOne.java
@@ -0,0 +1,11 @@
+package Rush;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class RushOne {
+    public static void Rush() throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
