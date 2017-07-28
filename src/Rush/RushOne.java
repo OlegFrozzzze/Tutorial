0684e0927cc53c396748a15c2753afb8e33fc00e
Frozzzze <ji900@yandex.ru>
2017-07-28 22:49:46 +0300

    Добавил условие для переменных

--- src/Rush/RushOne.java
+++ src/Rush/RushOne.java
@@ -8,6 +8,10 @@ public class RushOne {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int a = Integer.parseInt(reader.readLine());
         int b = Integer.parseInt(reader.readLine());
-        
+
+        if (a > 0 && b > 0) System.out.println(1);
+        if (a < 0 && b > 0) System.out.println(2);
+        if (a < 0 && b < 0) System.out.println(3);
+        if (a > 0 && b < 0) System.out.println(4);
     }
 }
