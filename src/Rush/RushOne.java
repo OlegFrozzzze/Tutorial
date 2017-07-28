83093880d3658fe8ec63335bf136fef981c0b4ec
Frozzzze <ji900@yandex.ru>
2017-07-28 23:03:52 +0300

    Решил

--- src/Rush/RushOne.java
+++ src/Rush/RushOne.java
@@ -7,7 +7,10 @@ public class RushOne {
     public static void Rush() throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Введите числа");
+
+        System.out.print("Введите число: ");
         int a = Integer.parseInt(reader.readLine());
+        System.out.print("Введите число: ");
         int b = Integer.parseInt(reader.readLine());
 
         if (a > 0 && b > 0) System.out.println(1);
