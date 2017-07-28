e0fe4061a5520f1c0703ed906d2192f07fe940d8
Frozzzze <ji900@yandex.ru>
2017-07-28 22:48:25 +0300

    Добавил переменные

--- src/Rush/RushOne.java
+++ src/Rush/RushOne.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 public class RushOne {
     public static void Rush() throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        
     }
 }
