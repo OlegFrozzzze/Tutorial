4e9f4328f6f742bc34c1b70d7a692ac60064c663
Frozzzze <ji900@yandex.ru>
2017-07-29 22:09:14 +0300

    Добавил переменные и условия

--- src/Rush/RushFour.java
+++ src/Rush/RushFour.java
@@ -7,5 +7,17 @@ import java.io.InputStreamReader;
 public class RushFour {
     public static void Rush() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        int x = 0;
+        if (a > 0)
+            x++;
+        if (b > 0)
+            x++;
+        if (c > 0)
+            x++;
+        System.out.print(x);
     }
 }
