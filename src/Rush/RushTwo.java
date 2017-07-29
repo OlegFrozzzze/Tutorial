f58a89462823bc9f52ed3d448e9d2bcffb82cc70
Frozzzze <ji900@yandex.ru>
2017-07-29 21:14:35 +0300

    Добавил две переменные

--- src/Rush/RushTwo.java
+++ src/Rush/RushTwo.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 public class RushTwo {
     public static void Rush() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        
+        int a = Integer.parseInt(reader.readLine());
+        boolean b = ((a % 2) == 0);
     }
 }
