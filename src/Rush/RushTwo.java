2b939e7bde38856e6e24fdbbcde163f64a0ab6e3
Frozzzze <ji900@yandex.ru>
2017-07-29 21:16:39 +0300

    Добавил условия

--- src/Rush/RushTwo.java
+++ src/Rush/RushTwo.java
@@ -9,5 +9,17 @@ public class RushTwo {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int a = Integer.parseInt(reader.readLine());
         boolean b = ((a % 2) == 0);
+
+        if (a == 0) {
+            System.out.println("ноль");
+        } else if (a < 0 && b) {
+            System.out.println("отрицательное четное число");
+        } else if (a > 0 && b) {
+            System.out.println("положительное четное число");
+        }else if (a < 0 && !b) {
+            System.out.println("отрицательное нечетное число");
+        } else  if(a > 0 && !b) {
+            System.out.println("положительное нечетное число");
+        }
     }
 }
