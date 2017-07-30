1490078b5ce7ab34445aa4536fb18da0c8800278
Frozzzze <ji900@yandex.ru>
2017-07-30 23:38:24 +0300

    Создал новый класс

---
+++ src/Rush/RushFive.java
@@ -0,0 +1,11 @@
+package Rush;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class RushFive {
+    public static void Rush(){
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+}
