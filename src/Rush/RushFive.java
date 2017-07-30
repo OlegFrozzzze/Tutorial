5cdf0a8b5487384426371ab8d3e5f89aa9e2d045
Frozzzze <ji900@yandex.ru>
2017-07-30 23:44:46 +0300

    Добавил переменные

--- src/Rush/RushFive.java
+++ src/Rush/RushFive.java
@@ -1,11 +1,17 @@
 package Rush;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class RushFive {
-    public static void Rush(){
+    public static void Rush() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        
+
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+
+       
     }
 }
