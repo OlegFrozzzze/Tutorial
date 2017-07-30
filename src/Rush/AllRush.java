c81c17214a5617954d2e81177ab96919efaa6302
Frozzzze <ji900@yandex.ru>
2017-07-30 23:56:40 +0300

    Добавил условие

--- src/Rush/AllRush.java
+++ src/Rush/AllRush.java
@@ -1,8 +1,15 @@
 package Rush;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class AllRush {
     public static void Allrush() throws Exception {
-        RushOne.Rush();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        if (a == 1) {
+            RushOne.Rush();
+        }
         RushTwo.Rush();
         RushThree.Rush();
         RushFour.Rush();
