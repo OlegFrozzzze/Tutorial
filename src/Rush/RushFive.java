d1b869e667fc650c14d0a29685abc5f4e0e7ca81
Frozzzze <ji900@yandex.ru>
2017-07-30 23:51:00 +0300

    Добавил условие

--- src/Rush/RushFive.java
+++ src/Rush/RushFive.java
@@ -12,6 +12,25 @@ public class RushFive {
         int b = Integer.parseInt(reader.readLine());
         int c = Integer.parseInt(reader.readLine());
 
-       
+        int z = 0;
+        int x = 0;
+
+        if (a != 0) {
+            if (a > 0) z++;
+            else x++;
+        }
+
+        if (b != 0) {
+            if (b > 0) z++;
+            else x++;
+        }
+
+        if (c != 0) {
+            if (c > 0) z++;
+            else x++;
+        }
+
+        System.out.println("количество положительных чисел: " + z);
+        System.out.println("количество отрицательных чисел: " + x);
     }
 }
