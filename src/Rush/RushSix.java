b1688a6e3bc56e98274a21fdb4fe0c5638db60f1
Frozzzze <ji900@yandex.ru>
2017-07-31 23:53:24 +0300

    Добавил переменные

--- src/Rush/RushSix.java
+++ src/Rush/RushSix.java
@@ -6,6 +6,13 @@ import java.io.InputStreamReader;
 public class RushSix {
     public static void Rush() throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        
+        System.out.print("getX: ");
+        int getX = Integer.parseInt(reader.readLine());
+        System.out.print("getY: ");
+        int getY = Integer.parseInt(reader.readLine());
+        System.out.print("toX: ");
+        int toX = Integer.parseInt(reader.readLine());
+        System.out.print("toY: ");
+        int toY = Integer.parseInt(reader.readLine());
     }
 }
