d3ea2f7647a57949f78799094f6213d2d10f2d17
Frozzzze <ji900@yandex.ru>
2017-07-31 23:59:59 +0300

    Добавил метод

---
+++ src/Rush/RushSeven.java
@@ -0,0 +1,12 @@
+package Rush;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RushSeven {
+    public static void Rush() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+}
