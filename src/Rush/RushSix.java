f0e82e2833a082a217b10a58dfb34fd52a1953ca
Frozzzze <ji900@yandex.ru>
2017-07-31 23:52:37 +0300

    Создал новый метод

---
+++ src/Rush/RushSix.java
@@ -0,0 +1,11 @@
+package Rush;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class RushSix {
+    public static void Rush() throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+}
