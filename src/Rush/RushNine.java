9bab7cb84b4dd5fde9c88d7c794f7b92650dd686
Frozzzze <ji900@yandex.ru>
2017-08-02 20:57:24 +0300

    Добавил переменные

--- src/Rush/RushNine.java
+++ src/Rush/RushNine.java
@@ -7,6 +7,12 @@ import java.io.InputStreamReader;
 public class RushNine {
     public static void Rush() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String name = reader.readLine();
+        int d = Integer.parseInt(reader.readLine());
+        int m = Integer.parseInt(reader.readLine());
+        int y = Integer.parseInt(reader.readLine());
+
         
     }
 }
