c6fddfe9ccc7e3fb4842923601e292dd584ab8a3
Frozzzze <ji900@yandex.ru>
2017-08-02 21:12:21 +0300

    Создал новый класс

---
+++ src/Rush/RushFromOneToTen/JRushOne.java
@@ -0,0 +1,12 @@
+package Rush.RushFromOneToTen;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class JRushOne {
+    public static void Jrush() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
