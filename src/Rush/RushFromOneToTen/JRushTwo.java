e316f6dcb0f84c57475bcef57c201b3663c0e3cb
Frozzzze <ji900@yandex.ru>
2017-08-03 18:31:20 +0300

    Добавил логический метод

--- src/Rush/RushFromOneToTen/JRushTwo.java
+++ src/Rush/RushFromOneToTen/JRushTwo.java
@@ -1,23 +1,27 @@
 package Rush.RushFromOneToTen;
 
 public class JRushTwo {
-    public static void JRush(){
+    public static void JRush() {
 
     }
-    public class Cat{
+
+    public class Cat {
         String name;
         int age;
         int weight;
         int strength;
 
-        public Cat(String name, int age, int weight, int strength){
-            this.name=name;
-            this.age=age;
-            this.weight=weight;
-            this.strength=strength;
+        public Cat(String name, int age, int weight, int strength) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.strength = strength;
+        }
+
+        public boolean fight(Cat anotherCat) {
+            return true;
         }
     }
-
-
 }
+
 
