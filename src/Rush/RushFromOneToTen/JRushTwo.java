6e101a6536bb40a18381118ca4a6c6d94bdd9c57
Frozzzze <ji900@yandex.ru>
2017-08-03 19:04:21 +0300

    Добавил значение для котов

--- src/Rush/RushFromOneToTen/JRushTwo.java
+++ src/Rush/RushFromOneToTen/JRushTwo.java
@@ -2,10 +2,13 @@ package Rush.RushFromOneToTen;
 
 public class JRushTwo {
     public static void JRush() {
-
+        Cat cat1 = new Cat("Simon",3,10,10);
+        Cat cat2 = new Cat("Simon2",2,3,5);
+        System.out.println(cat1.fight(cat2));
+        System.out.println(cat2.fight(cat1));
     }
 
-    public class Cat {
+    public static class Cat {
         String name;
         int age;
         int weight;
@@ -19,12 +22,13 @@ public class JRushTwo {
         }
 
         public boolean fight(Cat anotherCat) {
-            int agePlus = this.age > anotherCat.age?1:0;
-            int weightPlus = this.weight>anotherCat.weight?1:0;
-            int strengthPlus= this.strength>anotherCat.strength?1:0;
+            int agePlus = this.age > anotherCat.age ? 1 : 0;
+            int weightPlus = this.weight > anotherCat.weight ? 1 : 0;
+            int strengthPlus = this.strength > anotherCat.strength ? 1 : 0;
 
             int score = agePlus + weightPlus + strengthPlus;
-            return true;
+
+            return score > 2;
         }
     }
 }
