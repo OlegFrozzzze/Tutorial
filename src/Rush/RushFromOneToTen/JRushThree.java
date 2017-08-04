d23d4c2260bec1d127f4901175fe007f6a3617fc
Frozzzze <ji900@yandex.ru>
2017-08-04 23:50:51 +0300

    Поправил

--- src/Rush/RushFromOneToTen/JRushThree.java
+++ src/Rush/RushFromOneToTen/JRushThree.java
@@ -1,20 +1,19 @@
 package Rush.RushFromOneToTen;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class JRushThree {
-    public static void JRush() {
-    }
+    public static void JRush() throws IOException {
 
-    public class init {
-        int a;
-        int b;
-        int c;
-        int e;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        int d = Integer.parseInt(reader.readLine());
 
-        public void init(int a, int b, int c, int e) {
-            this.a = a;
-            this.b = b;
-            this.c = c;
-            this.e = e;
-        }
+        for (int i = 0; i < a; i++)
+            System.out.print("-");
     }
 }
