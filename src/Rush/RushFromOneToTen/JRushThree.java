68f6981d63eda95f801502767a16ca45cb0bac71
Frozzzze <ji900@yandex.ru>
2017-08-04 23:13:15 +0300

    Создал новый метод

--- src/Rush/RushFromOneToTen/JRushThree.java
+++ src/Rush/RushFromOneToTen/JRushThree.java
@@ -1,10 +1,16 @@
 package Rush.RushFromOneToTen;
 
 public class JRushThree {
-    public static void JRush(){
+    public static void JRush() {
+    }
+    public class init {
         int a;
         int b;
         int c;
         int e;
+
+        public void init(int a, int b, int c, int e) {
+
+        }
     }
 }
