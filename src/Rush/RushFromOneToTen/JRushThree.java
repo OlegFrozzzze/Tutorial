59f3c708518def8068b2b588b747aa83884bfd9a
Frozzzze <ji900@yandex.ru>
2017-08-04 23:15:08 +0300

    Инициализировал

--- src/Rush/RushFromOneToTen/JRushThree.java
+++ src/Rush/RushFromOneToTen/JRushThree.java
@@ -3,6 +3,7 @@ package Rush.RushFromOneToTen;
 public class JRushThree {
     public static void JRush() {
     }
+
     public class init {
         int a;
         int b;
@@ -10,7 +11,10 @@ public class JRushThree {
         int e;
 
         public void init(int a, int b, int c, int e) {
-
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.e = e;
         }
     }
 }
