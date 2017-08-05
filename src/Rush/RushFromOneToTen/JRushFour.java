1dcfe7967076c217f72ce9961f205485d15e8cc2
Frozzzze <ji900@yandex.ru>
2017-08-05 23:44:57 +0300

    Поправил

--- src/Rush/RushFromOneToTen/JRushFour.java
+++ src/Rush/RushFromOneToTen/JRushFour.java
@@ -1,31 +1,25 @@
 package Rush.RushFromOneToTen;
 
 public class JRushFour {
-            int age;
-        String name;
-        char sex;
+    int age;
+    String name;
+    char sex;
 
-        public  JRushFour(String name)
-        {
-            this.name=name;
-            this.age=1;
-        }
+    public JRushFour(String name) {
+        this.name = name;
+        this.age = 1;
+    }
 
-        public  JRushFour(String name, int age)
-        {
-            this.name=name;
-            this.age=age;
-        }
+    public JRushFour(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
-        public  JRushFour(String name,int age, char sex)
-        {
-            this.name=name;
-            this.age=age;
-            this.sex=sex;
-        }
+    public JRushFour(String name, int age, char sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
 
-        public static void main(String[] args) {
-
-        }
 }
 
