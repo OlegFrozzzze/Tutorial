31292c79c35fa1a1302bebbc41396dd428f10fec
Frozzzze <ji900@yandex.ru>
2017-08-07 00:00:23 +0300

    Создал новые методы

--- src/Rush/RushFromOneToTen/JRushSeven.java
+++ src/Rush/RushFromOneToTen/JRushSeven.java
@@ -1,4 +1,27 @@
 package Rush.RushFromOneToTen;
 
+import java.io.IOException;
+import java.rmi.RemoteException;
+
 public class JRushSeven {
+    public static void JRush(String[] args) {
+        processExceptions(new JRushSeven());
+    }
+
+    public static void processExceptions(JRushSeven obj) {
+        
+    }
+
+    public void method1() throws IOException {
+        throw new IOException();
+    }
+
+    public void method2() throws NoSuchFieldException {
+        throw new NoSuchFieldException();
+    }
+
+    public void method3() throws RemoteException {
+        throw new RemoteException();
+    }
 }
+
