fc5269cc225cf02dfe7b1b0712a75a16a1c1731a
Frozzzze <ji900@yandex.ru>
2017-08-10 23:57:37 +0300

    Добавил JTextArea

--- src/NewP/Window.java
+++ src/NewP/Window.java
@@ -3,6 +3,7 @@ package NewP;
 import javax.swing.*;
 
 public class Window extends JFrame{
+    private JTextArea text;
     public  Window (){
         super("Window");
         setBounds(1000, 300, 500, 500);
