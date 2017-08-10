f6c976d69996c09c45820c5be6f791ed19b482a9
Frozzzze <ji900@yandex.ru>
2017-08-10 23:59:36 +0300

    Добавил JTextArea

--- src/NewP/Window.java
+++ src/NewP/Window.java
@@ -8,6 +8,9 @@ public class Window extends JFrame{
         super("Window");
         setBounds(1000, 300, 500, 500);
     }
+    private  void textArea(){
+        text = new JTextArea();
+    }
     public static void Window(){
         Window add = new Window();
         add.setVisible(true);
