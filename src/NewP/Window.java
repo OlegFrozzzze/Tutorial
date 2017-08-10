66f0ac47c40403cb76d5b0215427e0c79ebc3a77
Frozzzze <ji900@yandex.ru>
2017-08-10 23:28:36 +0300

    Создал новый метод

--- src/NewP/Window.java
+++ src/NewP/Window.java
@@ -3,5 +3,12 @@ package NewP;
 import javax.swing.*;
 
 public class Window extends JFrame{
-    
+    public  Window (){
+        super("Window");
+        setBounds(1000, 300, 500, 500);
+    }
+    public static void Window(){
+        Window add = new Window();
+        add.setVisible(true);
+    }
 }
