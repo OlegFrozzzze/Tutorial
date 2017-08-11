d08a915f011b0a88a9fbec0bcb3178b410bdeb43
Frozzzze <ji900@yandex.ru>
2017-08-11 23:53:24 +0300

    Закрытие приводит к закрытию терминала

--- src/NewP/Window.java
+++ src/NewP/Window.java
@@ -5,9 +5,11 @@ import java.awt.*;
 
 public class Window extends JFrame{
     private JTextArea text;
+    private JTextArea text2;
     private JButton button;
     public  Window (){
         super("Window");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setBounds(1000, 300, 500, 500);
         textArea();
     }
@@ -20,6 +22,8 @@ public class Window extends JFrame{
 
         text = new JTextArea();
         add(text,BorderLayout.SOUTH);
+        text2 = new JTextArea();
+        add(text2, BorderLayout.WEST);
     }
     public static void Window(){
         Window add = new Window();
