62d75011389b70098ecfc85a27019e04ada72cd2
Frozzzze <ji900@yandex.ru>
2017-09-10 23:54:09 +0300

    Created a object a class BufferedReader Created a object a class ArrayList

--- src/StepickTets/ArrayTutorial.java
+++ src/StepickTets/ArrayTutorial.java
@@ -1,7 +1,16 @@
 package StepickTets;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+
 public class ArrayTutorial {
     public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList list = new ArrayList();
+
 
     }
 }
